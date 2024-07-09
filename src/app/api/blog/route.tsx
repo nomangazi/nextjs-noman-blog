b934ab8e63b82a82a1991537c1a1bfe2e4c0a020
Noman Gazi <numan.islam61@gmail.com>
2024-07-09 12:42:47 +0600

    update

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,16 +1,17 @@
 import { db } from "@/drizzle/db"
 import { user } from "@/drizzle/schema"
+import { eq } from "drizzle-orm"
 import { NextResponse } from "next/server"
 
 
 
 export const GET = async () => {
     try {
-        const blogs = await db.select().from(user)
+        const blogs = await db.select().from(user).where(eq(user.time_deleted, 1))
 
         console.log(blogs)
 
-        return NextResponse.json({ blogs }, { status: 200, statusText: "ok" })
+        return NextResponse.json({ error: 0, message: "List of Blog", data: blogs }, { status: 200 })
     } catch (ex: any) {
         return NextResponse.json({ "message": ex.message }, { status: 200 })
     }
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,10 +4,10 @@ import mysql from "mysql2/promise"
 import * as schema from "./schema"
 
 export const poolConn = await mysql.createConnection({
-    host: process.env.DB_HOST as string,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME as string,
+    host: process.env["DB_HOST"],
+    user: process.env["DB_USER"],
+    password: process.env["DB_PASSWORD"],
+    database: process.env["DB_NAME"],
     port: 3306,
     ssl: {
         rejectUnauthorized: false
